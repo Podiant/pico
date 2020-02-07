c76b987ae8ee6438e6a7d6292b79b2b32520219e
Mark Steadman <mark@bloomsbury.digital>
2020-02-07 11:34:02 +0000

    Adds evidence categories to the deliverable-detail view

--- pico/projects/views/deliverables.py
+++ pico/projects/views/deliverables.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic.detail import DetailView
 from pico.core.mixins import SiteMixin
-from ..models import Deliverable
+from ..models import Deliverable, TaskEvidenceTag, EvidenceCategory
 
 
 class DeliverableDetailView(SiteMixin, PermissionRequiredMixin, DetailView):
@@ -24,11 +24,20 @@ class DeliverableDetailView(SiteMixin, PermissionRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         obj = self.object
-        manager = obj.project.managers.get(
-            user=self.request.user
-        )
+
+        evidence_tags = TaskEvidenceTag.objects.filter(
+            task__evidence_direction='up',
+            task__deliverable=obj
+        ).values_list(
+            'tag',
+            flat=True
+        ).distinct()
+
+        evidence_categories = EvidenceCategory.objects.filter(
+            tags__tag__in=evidence_tags
+        ).distinct()
 
         context = super().get_context_data(**kwargs)
-        context['available_tasks'] = obj.available_tasks(manager)
+        context['evidence_categories'] = evidence_categories
 
         return context
